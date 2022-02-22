d0333982c125644264f3d0d4271dfed891397d98
keka3avr <kekazavr228@gmail.com>
2022-02-23 00:38:14 +1100

    BrowserRouter changed on HashRouter

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import PageRoute from './components/routes/PageRoute';
 import MainRoute from './components/routes/MainRoute';
 
